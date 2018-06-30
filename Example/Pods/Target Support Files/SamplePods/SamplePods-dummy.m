d2a851b389ae4ca783a1cae87cb3f984f82667ae
channy <dev.faithpeople@gmail.com>
2018-06-30 18:10:27 +0900

    first upload

---
+++ Example/Pods/Target Support Files/SamplePods/SamplePods-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SamplePods : NSObject
+@end
+@implementation PodsDummy_SamplePods
+@end
---
+++ SamplePods/Classes/SampleTestPods.swift
@@ -0,0 +1,14 @@
+//
+//  SampleTestPods.swift
+//  Pods-SamplePods_Example
+//
+//  Created by 신규찬 on 2018. 6. 30..
+//
+
+import UIKit
+
+public class SampleTestPods: NSObject {
+    public func testLog(){
+        print("Hellow world Test ^^")
+    }
+}
